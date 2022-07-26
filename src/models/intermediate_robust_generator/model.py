e2809d1b98a9421543499c807c987f3c49301d66
Andrew Parry <2393265p@student.gla.ac.uk>
2022-07-26 20:57:44 +0100

    Changed to mean instead of one hot

--- src/models/intermediate_robust_generator/model.py
+++ src/models/intermediate_robust_generator/model.py
@@ -11,8 +11,6 @@ from src.models.layers.custom_layers import generator_block
 def distance_loss(y_true, weights, interim_preds):
     cce = tfk.losses.CategoricalCrossentropy()
     n = len(weights)
-
-    print("y true: {}, y preds: {}".format(y_true, interim_preds))
     
     norms = [tf.norm(weights[i] - weights[j]) for i in range(n) for j in range(n)]
     weight_sum = tf.reduce_sum(norms, name="Norm of Weight Diff") 
@@ -32,8 +30,10 @@ class generator_model(tfk.Model):
         self.generators = [generator_block(config.in_dim, config.scale, config.n_classes, i, config.intermediate) for i in range(config.n_gen)]
 
     def _max_proba(self, proba):
-        totals = tfm.reduce_sum(proba, axis=0)
-        probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[0], on_value=1, off_value=0, dtype=tf.float32), elems=totals)
+        #totals = tfm.reduce_sum(proba, axis=0)
+        #probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[0], on_value=1, off_value=0, dtype=tf.float32), elems=totals)
+
+        probs = tf.reduce_mean(proba, axis=0)
 
         return probs
 
