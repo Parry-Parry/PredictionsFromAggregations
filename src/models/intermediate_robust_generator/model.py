a592ed153bc39ee9244d79cf642a74152905a69e
Andrew Parry <2393265p@student.gla.ac.uk>
2022-07-26 20:22:10 +0100

    Dtype changes

--- src/models/intermediate_robust_generator/model.py
+++ src/models/intermediate_robust_generator/model.py
@@ -28,7 +28,7 @@ class generator_model(tfk.Model):
 
     def _max_proba(self, proba):
         totals = tfm.reduce_sum(proba, axis=0)
-        probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[0], on_value=1, off_value=0), elems=totals)
+        probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[0], on_value=1, off_value=0, dtype=tf.float32), elems=totals)
 
         return probs
 
--- src/models/lstm_based/base_model.py
+++ src/models/lstm_based/base_model.py
@@ -29,7 +29,7 @@ class n_epsilon_model(tfk.Model):
 
     def _max_proba(self, proba):
         totals = tfm.reduce_sum(proba, axis=0)
-        probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[-1], on_value=1, off_value=0), elems=totals)
+        probs = tf.map_fn(lambda x : tf.one_hot(tf.argmax(x), depth=x.shape[-1], on_value=1, off_value=0, dtype=tf.float32), elems=totals)
 
         return probs
 
