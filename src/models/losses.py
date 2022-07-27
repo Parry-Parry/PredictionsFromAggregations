4a932a4f256934972a0c4bafd246dc22f8707f66
Andrew <2393265p@student.gla.ac.uk>
2022-07-27 05:51:54 +0100

    Updated loss function with normalisation

--- src/models/losses.py
+++ src/models/losses.py
@@ -7,11 +7,15 @@ def distance_loss(y_true, weights, interim_preds):
     n = len(weights)
     
     norms = [tf.norm(weights[i] - weights[j]) for i in range(n) for j in range(n)]
+    norm_max = norms[tfm.argmax(norms)]
+    norm_min = norms[tfm.argmin(norms)]
+
+    norms = [(norm - norm_min) / (norm_max - norm_min) for norm in norms]
     weight_sum = tf.reduce_sum(norms, name="Norm of Weight Diff") 
     cce_sum = tfm.reduce_sum(tf.map_fn(lambda x : cce(y_true, x), elems=interim_preds), axis=0, name="Sum of CE over Generated Preds")
 
-    return cce_sum 
-    #return cce_sum - weight_sum
+    #return cce_sum 
+    return cce_sum - weight_sum
 
 def ensemble_loss(y_true, interim_preds):
     cce = tfk.losses.CategoricalCrossentropy()
